0076f9e11b8afb06a93634db0f442de19552a859
CodinCat <a55951234@gmail.com>
2017-04-06 19:02:28 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  devtool: '#eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
